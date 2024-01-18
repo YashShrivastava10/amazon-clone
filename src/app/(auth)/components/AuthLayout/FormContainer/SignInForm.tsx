8cb8b8f19159bdcf41a9de365afef74c05bfa88e
YashShrivastava10 <shrivastavayash10@gmail.com>
2024-01-18 14:16:42 +0530

    Error UI for SignIn/Up page

--- src/app/(auth)/components/AuthLayout/FormContainer/SignInForm.tsx
+++ src/app/(auth)/components/AuthLayout/FormContainer/SignInForm.tsx
@@ -7,6 +7,7 @@ import React, { FormEvent, ReactElement, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import FieldError from '../ErrorContainer/FieldError';
 import Error from '../ErrorContainer/Error';
+import { useFormStatus } from 'react-dom';
 
 export type ErrorType = {
   status: boolean,
@@ -14,50 +15,69 @@ export type ErrorType = {
   message: string,
 }
 
-const SignInForm = ({children}: {children : ReactElement}) => {
+const SignInForm = ({ children }: { children: ReactElement }) => {
 
   const dispatch = useDispatch()
 
+
   const [show, setShow] = useState<string>("email")
   const [email, setEmail] = useState<string | undefined>("")
-  const [error, setError] = useState<ErrorType>({status: false, type: 1, message: ""})
+  const [error, setError] = useState<ErrorType>({ status: false, type: 1, message: "" })
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
-    const emailField = document.querySelector<HTMLInputElement>("#email")
-    const email = emailField?.value
-    if (email && email.trim() !== "") {
-      setEmail(email)
-      const status = await checkUser(email)
-      if (status) {
-        setShow("password")
-      }
-      else{
-        setError({status: true, type: 2, message: "We cannot find an account with that email address"})
-      }
-    }
-    else {
-      emailField?.focus()
-      setError({status: true, type: 1, message: "Enter your email or mobile phone number"})
-    }
+
   }
 
   const handleInput = () => {
-    if(error.status && error.type === 1){
+    if (error.status && error.type === 1) {
       const emailField = document.querySelector<HTMLInputElement>("#email")?.value
-      if(emailField){
-        setError({status: false, type: 1, message: ""})
+      if (emailField) {
+        setError({ status: false, type: 1, message: "" })
       }
-    }    
+    }
   }
 
+  const Continue = () => {
+    return (
+      <div className='w-full'>
+        <button type="submit" className='authBtn'>Continue</button>
+      </div>
+    )
+  }
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus()
+    return (
+      <div className='w-full'>
+        <button type="submit" className='w-full bg-yellow rounded-md h-[30px] text-13 shadow-md hover:bg-[#F7CA00]'>Sign in</button>
+      </div>
+    )
+  }
   if (show === "email") {
     return (
       <div className='flex flex-col gap-4'>
 
         {(error.status && error.type === 2) && <Error message={error.message} />}
 
-        <form onSubmit={handleSubmit} className='border border-silver rounded-lg flex flex-col justify-start items-start gap-4 py-4 px-5 w-full'>
+        <form action={async formData => {
+          const emailField = document.querySelector<HTMLInputElement>("#email")
+          const email = formData.get("email") as string
+          if (email && email.trim() !== "") {
+            setEmail(email)
+            const status = await checkUser(email)
+            if (status) {
+              setShow("password")
+            }
+            else {
+              setError({ status: true, type: 2, message: "We cannot find an account with that email address" })
+            }
+          }
+          else {
+            emailField?.focus()
+            setError({ status: true, type: 1, message: "Enter your email or mobile phone number" })
+          }
+        }} className='border border-silver rounded-lg flex flex-col justify-start items-start gap-4 py-4 px-5 w-full'>
           <div>
             <span className='text-3xl'>Sign in</span>
           </div>
@@ -66,9 +86,7 @@ const SignInForm = ({children}: {children : ReactElement}) => {
             <input id="email" type="text" name="email" onInput={handleInput} className={`${(error.status && error.type === 1) ? 'error' : 'inputField'}`} autoFocus />
             {(error.status && error.type === 1) && <FieldError message={error.message} />}
           </div>
-          <div className='w-full'>
-            <button type="submit" className='w-full bg-yellow rounded-md h-[30px] text-13 shadow-md hover:bg-[#F7CA00]'>Continue</button>
-          </div>
+          <Continue />
           <div>
             <p className='text-12 w-full'>By continuing, you agree to Amazon&apos;s <a>Conditions of Use</a> and <a>Privacy Notice</a>.</p>
           </div>
@@ -76,7 +94,7 @@ const SignInForm = ({children}: {children : ReactElement}) => {
             <a className='text-13'>Forget Paswword</a>
           </div>
         </form>
-        {show === "email" && children }
+        {show === "email" && children}
       </div>
     )
   }
@@ -85,12 +103,15 @@ const SignInForm = ({children}: {children : ReactElement}) => {
     return (
       <form action={async formData => {
         const password = formData.get("password")
-        const response = await signin(password as string, email as string)
-        if (response.success) {
-          dispatch(setUser(response.data))
-          redirect("/")
-        }
-        else console.log(response.message)
+        setTimeout(async () => {
+          const response = await signin(password as string, email as string)
+          if (response.success) {
+            dispatch(setUser(response.data))
+            redirect("/")
+          }
+          else console.log(response.message)
+        }, 2000)
+
       }} className='border border-silver rounded-lg flex flex-col justify-start items-start gap-4 py-4 px-5 w-full'>
         <div>
           <span className='text-3xl'>Sign in</span>
@@ -98,7 +119,7 @@ const SignInForm = ({children}: {children : ReactElement}) => {
         <div>
           <p className='flex gap-1 text-13'>
             <span>{email}</span>
-            <a onClick={() => {setShow("email"); setEmail("")}}>Change</a>
+            <a onClick={() => { setShow("email"); setEmail("") }}>Change</a>
           </p>
         </div>
         <div className='labelInput'>
@@ -106,11 +127,9 @@ const SignInForm = ({children}: {children : ReactElement}) => {
             <label htmlFor='password'>Password</label>
             <a className='text-13'>Forget your paswword?</a>
           </div>
-          <input id="password" type="password" name="password" autoFocus />
+          <input id="password" type="password" name="password" className={`${(error.status && error.type === 1) ? 'error' : 'inputField'}`} autoFocus />
         </div>
-        <div className='w-full'>
-          <button type="submit" className='w-full bg-yellow rounded-md h-[30px] text-13 shadow-md hover:bg-[#F7CA00]'>Sign in</button>
-        </div>
+        <SignInButton />
       </form>
     )
   }
--- src/app/(auth)/components/AuthLayout/FormContainer/SignUpForm.tsx
+++ src/app/(auth)/components/AuthLayout/FormContainer/SignUpForm.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import { signup } from '@/actions/auth'
+import { SignUpFormData, checkUser, signup } from '@/actions/auth'
 import { setUser } from '@/store/slice/userSlice';
 import Link from 'next/link'
 import { redirect } from 'next/navigation';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { ErrorType } from './SignInForm';
-import Error from '../ErrorContainer/Error';
 import Image from 'next/image';
 import info from "../../../../../../public/info.png"
+import FieldError from '../ErrorContainer/FieldError';
+
+type Field = {
+  value: string;
+  validator: (value: string) => boolean;
+  errorMessage: string;
+  type: number;
+};
+
+type Fields = Record<string, Field>;
 
 const SignUpForm = () => {
 
@@ -17,63 +26,93 @@ const SignUpForm = () => {
 
   const [error, setError] = useState<ErrorType>({ status: false, type: 1, message: "" })
 
+  const validateName = (name: string) => name.trim() === ""
+  const validateEmail = (email: string) => email.trim() === ""
+  const validatePassword = (password: string) => password.trim() === "" || password.length < 6
+  const validateRePassword = (password: string) => password.trim() === ""
+
+  const handelSubmit = async(formData: FormData) => {
+    const { name, email, password, rePassword } = Object.fromEntries(formData.entries()) as SignUpFormData
+
+    const fields: Fields = {
+      "#name": { value: name, validator: validateName, errorMessage: "Enter your name", type: 1 },
+      "#email": { value: email, validator: validateEmail, errorMessage: "Enter your email or mobile phone number", type: 2 },
+      "#password": { value: password, validator: validatePassword, errorMessage: "Minimum 6 characters required", type: 3 },
+      "#rePassword": { value: rePassword, validator: validateRePassword, errorMessage: "Type your password again", type: 4 }
+    }
+
+    for (const [key, { value, validator, errorMessage, type }] of Object.entries(fields)) {
+      if (validator(value)) {
+        setError({ status: true, message: errorMessage, type })
+        document.getElementById(`${key}`)?.focus()
+        return
+      }
+    }
+    if (await checkUser(email)) {
+      setError({ status: true, message: "There's already an account with this email.", type: 5 })
+      document.getElementById("#emai")?.focus()
+      return
+    }
+    if (password !== rePassword) {
+      setError({ status: true, message: "Passwords must match", type: 6 })
+      document.getElementById("#rePassword")?.focus()
+      return
+    }
+    const response = await signup(formData)
+    if (response.success) {
+      dispatch(setUser(response.data))
+      redirect("/")
+    }
+    else {
+      setError({ status: true, type: 7, message: response.message })
+    }
+  }
+
   return (
-    <div className='flex flex-col gap-4'>
+    <form action={handelSubmit} className='border border-silver rounded-lg flex flex-col justify-start items-start gap-4 px-5 py-4 w-full'>
+      <div>
+        <span className='text-3xl'>Create account</span>
+      </div>
 
-      {(error.status && error.type === 2) && <Error message={error.message} />}
+      <div className='labelInput'>
+        <label htmlFor='name'>Your Name</label>
+        <input id="name" name="name" className={`${(error.status && error.type === 1) ? 'error' : 'inputField'}`} placeholder="First and last name" autoFocus />
+        {(error.status && error.type === 1) && <FieldError message={error.message} />}
+      </div>
 
-      <form action={async formData => {
-        const password = formData.get("password")
-        const rePassword = formData.get("rePassword")
-        if (password === rePassword) {
-          const response = await signup(formData)
-          if (response.success) {
-            dispatch(setUser(response.data))
-            redirect("/")
-          }
-          else {
-            setError({ status: true, type: 2, message: response.message })
-          }
-        }
-        else {
-          console.log("Password do not match");
-        }
-      }} className='border border-silver rounded-lg flex flex-col justify-start items-start gap-4 px-5 py-4 w-full'>
-        <div>
-          <span className='text-3xl'>Create account</span>
-        </div>
-        <div className='labelInput'>
-          <label htmlFor='name'>Your Name</label>
-          <input id="name" name="name" className={`${(error.status && error.type === 1) ? 'error' : 'inputField'}`} placeholder="First and last name" required autoFocus />
-        </div>
-        <div className='labelInput'>
-          <label htmlFor='email'>Mobile number or email</label>
-          <input id="email" name="email" className={`${(error.status && error.type === 1) ? 'error' : 'inputField'}`} required />
-        </div>
-        <div className='labelInput'>
-          <label htmlFor='password'>Password</label>
-          <input id="password" name="password" type="password" className={`${(error.status && error.type === 1) ? 'error' : 'inputField'}`} placeholder="At least 6 characters" required />
-          <div className='flex items-center gap-1'>
-            <Image src={info} alt="" height={15} width={15} />
-            <span className='text-12'>Passwords must be at least 6 characters.</span>
-          </div>
-        </div>
-        <div className='labelInput'>
-          <label htmlFor='rePassword'>Re-enter Password</label>
-          <input id="rePassword" name="rePassword" type="password" className={`${(error.status && error.type === 1) ? 'error' : 'inputField'}`} required />
-        </div>
-        <div className='w-full'>
-          <button type="submit" className='w-full bg-yellow rounded-md h-[30px] text-13 shadow-md hover:bg-[#F7CA00]'>Continue</button>
-        </div>
-        <div>
-          <p className='text-12 w-full'>By continuing, you agree to Amazon&apos;s <a>Conditions of Use</a> and <a>Privacy Notice</a>.</p>
-        </div>
-        <div>
-          <span className='text-13'>Already have an account? <Link href="/signin">Sign in</Link></span>
-        </div>
-      </form>
-    </div>
+      <div className='labelInput'>
+        <label htmlFor='email'>Mobile number or email</label>
+        <input id="email" name="email" className={`${(error.status && (error.type === 2 || error.type === 5)) ? 'error' : 'inputField'}`} />
+        {(error.status && (error.type === 2 || error.type === 5)) && <FieldError message={error.message} />}
+      </div>
 
+      <div className='labelInput'>
+        <label htmlFor='password'>Password</label>
+        <input id="password" name="password" type="password" className={`${(error.status && (error.type === 3 || error.type === 6)) ? 'error' : 'inputField'}`} placeholder="At least 6 characters" />
+        {(error.status && error.type === 3) ? <FieldError message={error.message} /> : <div className='flex items-center gap-1'>
+          <Image src={info} alt="" height={15} width={15} />
+          <span className='text-12'>Passwords must be at least 6 characters.</span>
+        </div>}
+      </div>
+
+      <div className='labelInput'>
+        <label htmlFor='rePassword'>Re-enter Password</label>
+        <input id="rePassword" name="rePassword" type="password" className={`${(error.status && (error.type === 4 || error.type === 6)) ? 'error' : 'inputField'}`} />
+        {(error.status && (error.type === 4 || error.type === 6)) && <FieldError message={error.message} />}
+      </div>
+
+      <div className='w-full'>
+        <button type="submit" className='authBtn'>Continue</button>
+      </div>
+
+      <div>
+        <p className='text-12 w-full'>By continuing, you agree to Amazon&apos;s <a>Conditions of Use</a> and <a>Privacy Notice</a>.</p>
+      </div>
+      
+      <div>
+        <span className='text-13'>Already have an account? <Link href="/signin">Sign in</Link></span>
+      </div>
+    </form>
   )
 }
 
